1ba9a9706612e875b67174c7004b59aa6877d61e
shofiyul fuad <shofiyulfuad797@gmail.com>
2024-01-27 16:59:14 +0700

    final

--- app/page.js
+++ app/page.js
@@ -4,11 +4,6 @@ export default function Home() {
   return (
     <>
       <TopicsList />
-      <div>
-        <h1>Shofiyul Fuad</h1>
-        <p>Hello</p>
-        <p>lagi</p>
-      </div>
     </>
   );
 }
