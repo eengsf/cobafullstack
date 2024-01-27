3a8c249a9930db8c6e611b51cf30606da51a6816
shofiyul fuad <shofiyulfuad797@gmail.com>
2024-01-27 16:53:28 +0700

    lagi bro

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-// import TopicsList from "@/components/TopicsList";
+import TopicsList from "@/components/TopicsList";
 
 export default function Home() {
   return (
     <>
-      {/* <TopicsList /> */}
+      <TopicsList />
       <div>
         <h1>Shofiyul Fuad</h1>
         <p>Hello</p>
